e10f8ed27f7da3428e350b1b7d2ba749d16c1cce
ALyu <amy.lyu@turners.co.nz>
2023-07-16 19:37:49 +1200

    Setup angular workspace

---
+++ code/ui/projects/ngx-lib/src/lib/ngx-lib.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-lib',
+  template: `
+    <p>
+      ngx-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgxLibComponent {
+
+}
