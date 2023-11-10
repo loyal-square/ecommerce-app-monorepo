e12090048c819da8f139546266171578e0f1b880
Christopher Sy Alba <chris.alba.dev@gmail.com>
2023-11-10 21:50:10 +1300

    fix ngxlib import

--- code/ui/projects/loyal-square/src/app/app.module.ts
+++ code/ui/projects/loyal-square/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgxLibModule } from 'projects/ngx-lib/src/public-api';
+import { NgxLibModule } from 'ngx-lib';
 
 @NgModule({
   declarations: [AppComponent],
