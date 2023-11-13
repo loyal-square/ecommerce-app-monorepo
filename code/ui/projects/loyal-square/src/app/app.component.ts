53b26a920b0036cf959e89d2efe3ce8477c3d77e
Christopher Sy Alba <chris.alba.dev@gmail.com>
2023-11-14 00:09:11 +1300

    get theme switching to work

--- code/ui/projects/loyal-square/src/app/app.component.ts
+++ code/ui/projects/loyal-square/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from './services/themes/theme.service';
+import jss, { Classes } from 'jss';
+import { ThemeStructure } from './services/themes/theme.types';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'loyalSquare';
+  classes: Classes<keyof ThemeStructure> | undefined;
+  constructor(private themeService: ThemeService){}
+  public ngOnInit(): void {
+    this.themeService.init();
+    this.extractClassesFromThemeService();
+  }
+  private extractClassesFromThemeService(){
+    this.classes = this.themeService.themeClasses;
+  }
+  public toggleTheme(){
+    this.themeService.toggleLightDarkTheme();
+    this.extractClassesFromThemeService();
+    console.log(this.classes);
+    
+  }
 }
---
+++ code/ui/projects/loyal-square/src/app/services/themes/theme.service.ts
@@ -0,0 +1,95 @@
+import { Injectable, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ThemeColors, ThemeStructure } from './theme.types';
+import jss, { Classes, StyleSheet } from 'jss';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  public currentTheme: ThemeColors | undefined;
+  public themeClasses: Classes<keyof ThemeStructure> | undefined;
+  public loyalsquareStyles: ThemeStructure | undefined;
+  public sheet: StyleSheet<keyof ThemeStructure> | undefined;
+
+  public init(): void {
+    this.currentTheme = JSON.parse(
+      localStorage.getItem('theme') ?? JSON.stringify(loyalsquareDark)
+    );
+    this.changeColorTheme();
+    if (this.loyalsquareStyles) {
+      this.sheet = jss
+        .createStyleSheet(this.loyalsquareStyles, { link: true })
+        .attach()
+        .update(this.currentTheme ?? {});
+      this.themeClasses = this.sheet.classes;
+    }
+  }
+
+  public changeColorTheme() {
+    this.loyalsquareStyles = {
+      heading: {
+        color: this.currentTheme?.colors.secondary1 ?? 'black',
+        fontSize: this.currentTheme?.sizes?.headingSize ?? '2rem',
+        fontWeight: 700,
+        fontFamily: 'Arial, sans-serif',
+      },
+      subHeading: {
+        color: this.currentTheme?.colors.secondary1 ?? 'black',
+        fontSize: this.currentTheme?.sizes?.headingSize ?? '1.3rem',
+        fontWeight: 200,
+        fontFamily: 'Arial, sans-serif',
+      },
+      base: {
+        backgroundColor: (data) => data.colors.primary1 ?? 'white',
+        color: (data) => data.colors.secondary1 ?? 'black',
+        height: (data) => '100vh',
+        display: (data) => 'block',
+      },
+    };
+    if (this.sheet) {
+      this.sheet.update(this.currentTheme ?? {});
+      this.themeClasses = this.sheet.classes;
+    }
+  }
+
+  public toggleLightDarkTheme() {
+    if (this.currentTheme?.name === 'loyalsquare-light') {
+      this.currentTheme = loyalsquareDark;
+    } else {
+      this.currentTheme = loyalsquareLight;
+    }
+
+    this.changeColorTheme();
+  }
+
+  public overrideLoyalSquareTheme(customTheme: ThemeColors) {
+    this.currentTheme = customTheme;
+
+    this.changeColorTheme();
+
+    if (this.themeClasses) {
+      const sheet = jss
+        .createStyleSheet(this.themeClasses, { link: true })
+        .attach();
+      this.themeClasses = sheet.classes;
+    }
+  }
+}
+
+export const loyalsquareLight: ThemeColors = {
+  name: 'loyalsquare-light',
+  colors: {
+    primary1: 'white',
+    secondary1: 'black',
+    tertiary1: '#4387FF',
+  },
+};
+
+export const loyalsquareDark: ThemeColors = {
+  name: 'loyalsquare-dark',
+  colors: {
+    primary1: 'black',
+    secondary1: 'white',
+    tertiary1: '#4387FF',
+  },
+};
