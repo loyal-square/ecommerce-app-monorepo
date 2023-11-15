6fc08779c263367c40553dbb1dfb449dbc131b8c
Christopher Sy Alba <chris.alba.dev@gmail.com>
2023-11-16 12:38:25 +1300

    implement actual theme overrides and add logo in footer

--- code/ui/projects/loyal-square/src/app/components/footer/footer.component.ts
+++ code/ui/projects/loyal-square/src/app/components/footer/footer.component.ts
@@ -2,16 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { ThemeService } from '../../services/themes/theme.service';
 import { Classes } from 'jss';
 import { ThemeStructure } from '../../services/themes/theme.types';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent implements OnInit{
+export class FooterComponent implements OnInit {
   public classes: Classes<keyof ThemeStructure> | undefined;
-  constructor(public themeService:ThemeService){}
-  public ngOnInit():void {
+
+  constructor(public themeService: ThemeService) {}
+  public ngOnInit(): void {
     this.classes = this.themeService.themeClasses;
+  }
+
+  public getImageLogoUrl(): string {
+    return this.themeService.currentTheme?.name.includes('light')
+      ? '../../../assets/images/logoLight.png'
+      : '../../../assets/images/logoDark.png';
   }
 }
