f9b350b4e37d08a79b85699b4efcde638a32c0cb
Christopher Sy Alba <chris.alba.dev@gmail.com>
2023-11-16 12:39:20 +1300

    Update footer.component.ts

--- code/ui/projects/loyal-square/src/app/components/footer/footer.component.ts
+++ code/ui/projects/loyal-square/src/app/components/footer/footer.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { ThemeService } from '../../services/themes/theme.service';
 import { Classes } from 'jss';
 import { ThemeStructure } from '../../services/themes/theme.types';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
