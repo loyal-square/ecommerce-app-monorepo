367af0a6f60ca0b71c5e184d278f04e26bc93f8b
Christopher Sy Alba <christopher.alba@turners.co.nz>
2023-11-26 10:58:55 +1300

    create new components and routes

--- code/ui/projects/loyal-square/src/app/app-routing.module.ts
+++ code/ui/projects/loyal-square/src/app/app-routing.module.ts
@@ -1,10 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CareersComponent } from './pages/careers/careers.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HelpPageComponent } from './pages/help-page/help-page.component';
+import { AnnouncementsComponent } from './pages/announcements/announcements.component';
+import { VerifyAccountComponent } from './pages/verify-account/verify-account.component';
+import { ShellComponent } from './components/shell/shell.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'careers',
+        component: CareersComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: 'help',
+        component: HelpPageComponent,
+      },
+      {
+        path: 'announcements',
+        component: AnnouncementsComponent,
+      },
+    ],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'verify-account',
+    component: VerifyAccountComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- code/ui/projects/loyal-square/src/app/app.module.ts
+++ code/ui/projects/loyal-square/src/app/app.module.ts
@@ -8,12 +8,21 @@ import { NgxLibModule } from 'ngx-lib';
 import jss from 'jss';
 import jssPresetDefault from 'jss-preset-default';
 import jssDynamic from 'jss-plugin-rule-value-function';
-import { FooterComponent } from "./components/footer/footer.component"
+import { FooterComponent } from "./components/footer/footer.component";
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CareersComponent } from './pages/careers/careers.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HelpPageComponent } from './pages/help-page/help-page.component';
+import { AnnouncementsComponent } from './pages/announcements/announcements.component';
+import { VerifyAccountComponent } from './pages/verify-account/verify-account.component';
+import { ShellComponent } from './components/shell/shell.component';
+import { HomeComponent } from './components/home/home.component'
 jss.setup(jssPresetDefault());
 jss.use(jssDynamic());
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent],
+  declarations: [AppComponent, FooterComponent, LoginComponent, RegisterComponent, CareersComponent, AboutComponent, HelpPageComponent, AnnouncementsComponent, VerifyAccountComponent, ShellComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, NgxLibModule],
   providers: [],
   bootstrap: [AppComponent],
